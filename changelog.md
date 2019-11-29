97d1bcb79df967d2c071ee891b96a9def67f12f0
Brennan <boingyman@users.noreply.github.com>
2019-11-28 21:16:45 -0600

    Initial commit.

---
+++ changelog.md
@@ -0,0 +1,18 @@
+### Changelog
+
+##### Planned
+
+* Added feature to rename multiple individual files.
+* Removed flags `-r` and `--rename`.
+    * Renaming files is now default behavior without flags.
+* Removed flags `-i` and `--ignore-errors`.
+    * Feature removed entirely.
+* Removed flags `-t` and `--test-rename`.
+    * Feature removed entirely.
+* Changes to `isaacpost1424.py`:
+    * Removed `test_string` method.
+
+##### v1.0.0 [2019-11-23]
+
+- Initial release.
+- Includes `isaacpost1424.py`.
---
+++ isaacpost1424.py
@@ -0,0 +1,36 @@
+"""
+Renaming script for the Northernlion video series "The Binding of Isaac: AFTERBIRTH+ - Northernlion Plays"
+
+FORMAT IN: {Flavor Text} - The Binding of Isaac{':' or ' - '} AFTERBIRTH+ - Northernlion Plays - Episode {Number}
+FORMAT OUT: The Binding of Isaac{':' or ' - '} AFTERBIRTH+ - Northernlion Plays - Episode {####} [{Flavor Text}]
+"""
+
+import re
+
+
+def rename(name) -> str:
+
+    search_pattern = r"(?P<path>.*/)?(?P<flavor>.*?)\s\-\s(?P<body>The\sBinding\sof\sIsaac(?:(?:\s\-\s)|(?:\:\s))" + \
+                     r"AFTERBIRTH\+.*)\s\-\s(?P<episode>Episode\s(?P<episode_number>[0-9]*))(?P<ext>\..*)?"
+    s_obj = re.search(search_pattern, name, re.I)
+
+    result = ""
+
+    if s_obj is None:
+        raise Exception("Given file name did not match regex pattern:\n" + search_pattern + "\n")
+
+    result = s_obj.groupdict()['body'] + " - " + s_obj.groupdict()['episode'] + " [" + s_obj.groupdict()['flavor'] + "]"
+
+    # Checks for 'ext' capture group, as it is the file extension, if it exists.
+    if not s_obj.groupdict()['ext'] is None:
+        result = result + s_obj.groupdict()['ext']
+
+    # Checks for 'path' capture group, as it is the path to the file, if it exists.
+    if not s_obj.groupdict()['path'] is None:
+        result = s_obj.groupdict()['path'] + result
+
+    return result
+
+
+def test_string() -> str:
+    return 'Lion the Egg - The Binding of Isaac: AFTERBIRTH+ - Northernlion Plays - Episode 4000.mp4'
